d5c60143b709ea55311e1a6ad9e42aafedbb290c
Skywalker8510 <91810623+Skywalker8510@users.noreply.github.com>
2024-03-30 10:58:05 -0500

    completed with loop, output and formating

--- src/Assignment3/IncomeTaxCalculator.java
+++ src/Assignment3/IncomeTaxCalculator.java
@@ -3,10 +3,11 @@ package Assignment3;
 //Class: INFO 1521 Spring 2024
 //Assignment: MCCJava1
 //Date: 3/28/2024
-//Resources: 
+//Resources: Canvas page, Stack overflow
 
 // Java program to Calculate income tax from user inputs
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -18,69 +19,117 @@ public class IncomeTaxCalculator {
         ArrayList<Integer> grossPayArray = new ArrayList<Integer>();
         ArrayList<Integer> children = new ArrayList<Integer>();
         ArrayList<Integer> otherDependents = new ArrayList<Integer>();
-        ArrayList<Double> tax = new ArrayList<Double>();
+        ArrayList<Double> taxArray = new ArrayList<Double>();
+        ArrayList<Integer> taxAmount = new ArrayList<Integer>();
+        DecimalFormat df = new DecimalFormat("###,###,###.##");
 
-        System.out.println("**** Income Tax Calculator ****");
+        String userInput = "y";
+        while(userInput.equalsIgnoreCase("y")) {
 
-        System.out.print("Enter number of people to calculate for: ");
-        int numberOfPeople = scan.nextInt();
-        System.out.println();
+            grossPayArray.clear();
+            children.clear();
+            otherDependents.clear();
+            taxArray.clear();
+            taxAmount.clear();
 
-        for(int i = 0; i < numberOfPeople; i++) {
+            System.out.println("**** Income Tax Calculator ****");
 
-            System.out.print("Enter gross pay for employee " + (i+1) + ": ");
-            int grossPay = scan.nextInt();
+            System.out.print("Enter number of people to calculate for: ");
+            int numberOfPeople = scan.nextInt();
+            System.out.println();
 
-            while(grossPay < 0) {
+            for (int i = 0; i < numberOfPeople; i++) {
 
-                System.out.println("Error, Pay must be positive.");
+                System.out.print("Enter gross pay for employee " + (i + 1) + ": ");
+                int grossPay = scan.nextInt();
 
-                System.out.print("Enter gross pay for employee " + (i+1) + ": ");
-                grossPay = scan.nextInt();
+                while (grossPay < 0) {
+
+                    System.out.println("Error, Pay must be positive.");
+
+                    System.out.print("Enter gross pay for employee " + (i + 1) + ": ");
+                    grossPay = scan.nextInt();
+                    System.out.println();
+
+                }
+
+                grossPayArray.add(grossPay);
+
+                if (grossPayArray.get(i) < 200000) {
+                    System.out.print("Enter number of children: ");
+                    children.add(scan.nextInt());
+                    System.out.print("Enter number of other dependents: ");
+                    otherDependents.add(scan.nextInt());
+                    System.out.println();
+
+                } else {
+                    children.add(0);
+                    otherDependents.add(0);
+                }
+
+                double tax;
+
+                if (0 <= grossPayArray.get(i) && grossPayArray.get(i) <= 11000) {
+                    tax = (grossPayArray.get(i) * 0.08);
+                    taxAmount.add(8);
+                } else if (11001 <= grossPayArray.get(i) && grossPayArray.get(i) <= 44725) {
+                    tax = (grossPayArray.get(i) * 0.14);
+                    taxAmount.add(14);
+                } else if (44726 <= grossPayArray.get(i) && grossPayArray.get(i) <= 95375) {
+                    tax = (grossPayArray.get(i) * 0.22);
+                    taxAmount.add(22);
+                } else if (95376 <= grossPayArray.get(i) && grossPayArray.get(i) <= 182100) {
+                    tax = (grossPayArray.get(i) * 0.25);
+                    taxAmount.add(25);
+                } else if (182101 <= grossPayArray.get(i) && grossPayArray.get(i) <= 231250) {
+                    tax = (grossPayArray.get(i) * 0.28);
+                    taxAmount.add(28);
+                } else if (231251 <= grossPayArray.get(i)) {
+                    tax = (grossPayArray.get(i) * 0.32);
+                    taxAmount.add(32);
+                } else {
+                    tax = 0.00;
+                    taxAmount.add(0);
+                }
+
+                tax -= (children.get(i) * 2000) - (otherDependents.get(i) * 500);
+                if (tax < 0) {
+                    tax = 0;
+                }
+
+                taxArray.add(tax);
+
+            }
+
+            for (int i = 0; i < grossPayArray.size(); i++) {
                 System.out.println();
+                System.out.println("*** Employee" + (i + 1) + "***");
+                System.out.println("Yearly Pay: $" + df.format(grossPayArray.get(i)));
+                System.out.println("Income Tax " + taxAmount.get(i) + "%: $" + df.format(taxArray.get(i)));
+
 
             }
 
-            grossPayArray.add(grossPay);
+            System.out.println();
+            System.out.println("Total Taxes: $" + df.format(taxArray.stream().mapToDouble(a -> a).sum()));
 
-            if(grossPayArray.get(i) < 200000) {
-                System.out.print("Enter number of children: ");
-                children.add(scan.nextInt());
-                System.out.print("Enter number of other dependents: ");
-                otherDependents.add(scan.nextInt());
-                System.out.println();
+            System.out.println();
+            System.out.print("Calculate another?(y/n): ");
+            userInput = scan.next();
+            System.out.println();
 
-            } else {
-                children.add(0);
-                otherDependents.add(0);
-            }
 
-            if(0 <= grossPayArray.get(i) && grossPayArray.get(i) <= 11000) {
-                tax.add(grossPayArray.get(i) * 0.08);
-            }else if (11001 <= grossPayArray.get(i) && grossPayArray.get(i) <= 44725) {
-                tax.add(grossPayArray.get(i) * 0.14);
-            }else if (44726 <= grossPayArray.get(i) && grossPayArray.get(i) <= 95375) {
-                tax.add(grossPayArray.get(i) * 0.22);
-            }else if (95376 <= grossPayArray.get(i) && grossPayArray.get(i) <= 182100) {
-                tax.add(grossPayArray.get(i) * 0.25);
-            }else if (182101 <= grossPayArray.get(i) && grossPayArray.get(i) <= 231250) {
-                tax.add(grossPayArray.get(i) * 0.28);
-            }else if (231251 <= grossPayArray.get(i)){
-                tax.add(grossPayArray.get(i) * 0.32);
-            }else {
 
-            }
+
+            //        //Testing Code
+            //        for(int i = 0; i < grossPay.size(); i++) {
+            //            System.out.println(grossPay.get(i));
+            //            System.out.println(children.get(i));
+            //            System.out.println(otherDependents.get(i));
+            //        }
+
 
         }
-
-
-
-//        //Testing Code
-//        for(int i = 0; i < grossPay.size(); i++) {
-//            System.out.println(grossPay.get(i));
-//            System.out.println(children.get(i));
-//            System.out.println(otherDependents.get(i));
-//        }
 
     }
 
