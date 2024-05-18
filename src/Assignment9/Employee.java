61ef9cdc7d90d9511f5d3ad6978c6b22a3eeaee1
Skywalker8510 <91810623+Skywalker8510@users.noreply.github.com>
2024-05-18 12:33:36 -0500

    Completed override methods

--- src/Assignment9/Employee.java
+++ src/Assignment9/Employee.java
@@ -18,6 +18,7 @@ public abstract class Employee {
     protected ArrayList<EmergencyContact> emergencyContacts;
 
     public Employee(String fn, String ln, int en, String dept, String job) {
+        percent.setMaximumFractionDigits(3);
         firstName = fn;
         lastName = ln;
         employeeNum = en;
@@ -29,6 +30,7 @@ public abstract class Employee {
     }
     
     public Employee(String fn, String ln, int en) {
+        percent.setMaximumFractionDigits(3);
         firstName = fn;
         lastName = ln;
         employeeNum = en;
@@ -40,6 +42,7 @@ public abstract class Employee {
     }
 
     public Employee(Employee e) {
+        percent.setMaximumFractionDigits(3);
         firstName = e.getFirstName();
         lastName = e.getLastName();
         employeeNum = e.getEmployeeNumber();
@@ -52,6 +55,7 @@ public abstract class Employee {
     
     // default
     public Employee() {
+        percent.setMaximumFractionDigits(3);
         firstName = "";
         lastName = "";
         employeeNum = 0;
@@ -194,12 +198,19 @@ public abstract class Employee {
 
     @Override
     public String toString() {
-        return super.toString();
+        return "Name: " + firstName + " " + lastName + "\nID: " + employeeNum + "\nDepartment: " + department + "\nTitle: " + jobTitle;
     }
 
     @Override
     public boolean equals(Object obj2) {
-
+        if (this == obj2) {
+            return true;
+        }
+        if (obj2 == null || getClass() != obj2.getClass()) {
+            return false;
+        }
+        Employee employee = (Employee) obj2;
+        return employeeNum == employee.employeeNum;
     }
 
 }
